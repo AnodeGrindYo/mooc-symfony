4585d2fe3632bda50c3284b6ef476fcc8ca4c2a4
adriengodoy <mr.a.godoy@gmail.com>
2018-04-10 23:32:21 +0200

    manipulation de l'objet Request et de l'objet Response dans le controller

--- Symfony/src/OC/PlatformBundle/Controller/AdvertController.php
+++ Symfony/src/OC/PlatformBundle/Controller/AdvertController.php
@@ -5,6 +5,7 @@
 namespace OC\PlatformBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
@@ -17,17 +18,15 @@ class AdvertController extends Controller
             array('id'=>5) //2e arg: valeurs des params
             // si on veut générer une url absolue, il faut un 3e param:
             // UrlGeneratorInterface::ABSOLUTE_URL
+            // ainsi, $url vaudra http://monsite.com/platform et pas uniquement /platform
         );
         // $url vaut "/platform/advert/5"
         
         return new Response("l'URL de l'annonce d'id 5 est : ".$url);
     }
     
-    // La route fait appel à OCPlatformBundle:Advert:view
-    // on doit donc définir la méthode viewAction.
-    // on donne à cette méthode l'argument $id, pour
-    // correspondre au paramètre {id} de la route
-    public function viewAction($id)
+    // On injecte la requête dans les arguments de la méthode
+    /*public function viewAction($id, Request $request) // on a maintenant accès à la requête HTTP via $request
     {
         // $id vaut 5 si l'on a appelé l'URL /platform/advert/5
         
@@ -36,7 +35,26 @@ class AdvertController extends Controller
         // puis on passera l'annonce à la vue pour qu'elle 
         // puisse l'afficher
         
-        return new Response("Affichage de l'annonce d'id : ".$id);
+        // récupération du paramètre tag:
+        $tag = $request->query->get('tag');
+        
+        return new Response("Affichage de l'annonce d'id : ".$id.", avec le tag : ".$tag);
+    }*/
+    
+    // manipulation de l'objet Response. Méthode longue:
+    public function viewAction($id)
+    {
+        // on crée la réponse sans lui donner de contenu pour le moment
+        $response = new Response();
+        
+        // on définit le contenu
+        $response->setContent("<marquee><h1>Ceci est une page d'erreur 404</h1></marquee>");
+        
+        // on définit le code HTTP à "not found" (erreur 404)
+        $response->setStatusCode(Response::HTTP_NOT_FOUND);
+        
+        // on retourne la réponse
+        return $response;
     }
     
     public function viewSlugAction($slug, $year, $format)

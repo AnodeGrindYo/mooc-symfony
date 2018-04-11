cd85b789a896affcaa020a7636b130cf2cd3e7a5
adriengodoy <mr.a.godoy@gmail.com>
2018-04-11 10:50:02 +0200

    dans AdvertController, maj de viewAction et de indexAction. Création des méthodes addAction, editAction et deleteAction

--- Symfony/src/OC/PlatformBundle/Controller/AdvertController.php
+++ Symfony/src/OC/PlatformBundle/Controller/AdvertController.php
@@ -7,35 +7,33 @@ namespace OC\PlatformBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdvertController extends Controller
 {
     public function indexAction()
     {
-        // on veut avoir l'url de l'annonce d'id 5
-        $url = $this->get('router')->generate(
-            'oc_platform_view', // 1er arg: nom de la route
-            array('id'=>5) //2e arg: valeurs des params
-            // si on veut générer une url absolue, il faut un 3e param:
-            // UrlGeneratorInterface::ABSOLUTE_URL
-            // ainsi, $url vaudra http://monsite.com/platform et pas uniquement /platform
-        );
-        // $url vaut "/platform/advert/5"
+        // on ne sait pas combien de pages il y a
+        // mais on sait qu'une page doit être supérieure ou égale à 1
+        if ($page < 1) {
+            // on déclenche une exception NotFoundHttpException, 
+            // cela va afficher une page d'erreur 404 (qu'on pourra personnaliser plus tard)
+            throw new NotFoundException('Page "'.$page.'" inexistante. :(');
+        }
         
-        return new Response("l'URL de l'annonce d'id 5 est : ".$url);
+        // ici on récupérera la liste des annonces, puis on la passera au template
+        
+        // Mais pour l'instant, on ne fait qu'appeler le template
+        return $this->render('OCPlatformBundle:Advert:index.html.twig');
     }
     
     // manip de la session
-    public function viewAction($id, Request $request)
+    public function viewAction($id)
     {
-        // récupération de la session
-        $session = $request->getSession();
-        
-        // on récupère le contenu de la variable user_id
-        $session->set('user_id', 91);
-        
-        // On n'oublie pas de renvoyer une réponse
-        return new Response("<body>je suis une page de test pas très loquace</body>");
+        // ici, on va récupérer l'annonce correspondant à l'id $id
+       return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+           'id' => $id
+       ));
     }
     
     public function viewSlugAction($slug, $year, $format)
@@ -43,5 +41,45 @@ class AdvertController extends Controller
         return new Response(
             "On pourrait afficher l'annonce correspondant au slug '".$slug."', créée en ".$year." et au format ".$format."."
         );
+    }
+    
+    public function addAction(Request $request)
+    {
+        // la gestion d'un formulaire est particulière, mais l'idée set la suivante :
+        
+        // Si la requête est en POST, c'est que le visiteur a soumis le formulaire
+        if ($request->isMethod('POST')) {
+            // ici, on s'occupera de la création et de la gestion du formulaire
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+            
+            // puis on redirige vers la page de visualisation de cette annonce
+            return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+        }
+        
+        // s ion est pas en POST, alors on affiche le formulaire
+        return $this->render('OCPlatformBundle:Advert:add.html.twig');
+    }
+    
+    public function editAction($id, Request $request)
+    {
+        // ici, on récupérera l'annonce correspondant à $id
+        
+        // même mécanisme que pour l'ajout
+        if ($request->isMethod('POST')) {
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
+            
+            return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+        }
+        
+        return $this->render('OCPlatformBundle:Advert:edit.html.twig');
+    }
+    
+    public function deleteAction($id)
+    {
+        // ici, on récupérera l'annonce correspondant à l'$id
+        
+        // ici, on générera la suppression de l'annonce en question
+        
+        return $this->render('OCPlatformBundle:Advert:delete.html.twig');
     }
 }
